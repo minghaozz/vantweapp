59379160875d27aa47b13e4f15c270a71e25da53
mingc99 <1095058962@qq.com>
2020-06-23 14:06:35 +0800

    Create check.vue

---
+++ check.vue
@@ -0,0 +1,7 @@
+            <van-popup :show="showGoOnTimePicker"  position="bottom">
+                <van-datetime-picker type="time" value="6:00"
+                                     @cancel="onCancelDatePicker"
+                                     @confirm="onConfirmGoOnTimePicker"
+                                     :formatter="formatter"
+                />
+            </van-popup>
